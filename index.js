77fc496d210b597a1ed924eadbba8f1d3a651f00
Rashmi <165359201+rashmibarodiya@users.noreply.github.com>
2024-05-06 09:40:30 +0000

    added username using id

--- index.js
+++ index.js
@@ -2,21 +2,25 @@
 const express = require("express");
 const http = require("http")
 const app = express();
-const {Server} = require("socket.io")
-const server= http.createServer(app)
+const { Server } = require("socket.io")
+const server = http.createServer(app)
 const io = new Server(server)
 const { join } = require('node:path');
 
-app.get('/', (req, res) => {
-    res.sendFile(join(__dirname, 'index.html'));
-  });
 
-  io.on("connection", (client) => {
-    client.on("userMsg",(mes) => {
-        io.emit("message",mes)
-    })
-  })
-  
-  server.listen(3000, () => {
-    console.log('server running at http://localhost:3000');
+app.get('/', (req, res) => {
+  res.sendFile(join(__dirname, 'index.html'));
+});
+
+
+io.on("connection", (client) => {
+  let username = `User${client.id.slice(0, 5)}`; // when user connects on io it has a .id here i took only five intials of that id
+  client.on("userMsg",(message) => {
+      io.emit("message", `${username}: ${message}`);
   });
+});
+
+
+server.listen(3000, () => {
+  console.log('server running at http://localhost:3000');
+});
